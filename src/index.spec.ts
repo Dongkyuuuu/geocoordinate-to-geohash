8d5cc1ec71c9a6dade0cfc239b0cff7966c0dda1
dongkyuuuu <desktop@kakao.com>
2023-09-16 12:57:50 +0900

    test: add testcase

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,1 +1,9 @@
 import { geocoordinateToGeohash } from "./";
+
+it("geocoordinateToGeohash", () => {
+  const latitude = 37.56616;
+  const longitude = 126.978356;
+  const geohash = geocoordinateToGeohash(latitude, longitude);
+
+  expect(geohash).toBe("wydm9qwydpu9");
+});
