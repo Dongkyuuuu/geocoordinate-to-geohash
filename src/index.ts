efb7f4fead2b985ea145204675dab992f3cd5e0e
dongkyuuuu <desktop@kakao.com>
2023-09-16 12:28:41 +0900

    fix: remove hash to coord

--- src/index.ts
+++ src/index.ts
@@ -44,39 +44,3 @@ export function geocoordinateToGeohash(
 
   return geohash;
 }
-
-export function geohashToGeocoordinate(geohash: string): [number, number] {
-  let isEven = true;
-  let latMin = -90,
-    latMax = 90;
-  let lonMin = -180,
-    lonMax = 180;
-  let lat = 0,
-    lon = 0;
-
-  for (let i = 0; i < geohash.length; i++) {
-    const ch = BASE32.indexOf(geohash[i]);
-    for (let j = 0; j < 5; j++) {
-      const mask = BITS[j];
-      if (isEven) {
-        if ((ch & mask) !== 0) {
-          lonMin = (lonMin + lonMax) / 2;
-        } else {
-          lonMax = (lonMin + lonMax) / 2;
-        }
-      } else {
-        if ((ch & mask) !== 0) {
-          latMin = (latMin + latMax) / 2;
-        } else {
-          latMax = (latMin + latMax) / 2;
-        }
-      }
-      isEven = !isEven;
-    }
-  }
-
-  lat = (latMin + latMax) / 2;
-  lon = (lonMin + lonMax) / 2;
-
-  return [lat, lon];
-}
