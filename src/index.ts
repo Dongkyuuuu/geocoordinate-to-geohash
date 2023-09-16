18b7f424816ecc78fd3d9dfa8e163275141c64a5
dongkyuuuu <desktop@kakao.com>
2023-09-16 12:21:20 +0900

    feat: geohashToGeocoordinate

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { BASE32, BITS } from "./constants";
 
-export function convertToGeohash(latitude: number, longitude: number): string {
+export function geocoordinateToGeohash(
+  latitude: number,
+  longitude: number
+): string {
   let isEven = true;
   let latMin = -90,
     latMax = 90;
@@ -40,4 +43,40 @@ export function convertToGeohash(latitude: number, longitude: number): string {
   }
 
   return geohash;
+}
+
+export function geohashToGeocoordinate(geohash: string): [number, number] {
+  let isEven = true;
+  let latMin = -90,
+    latMax = 90;
+  let lonMin = -180,
+    lonMax = 180;
+  let lat = 0,
+    lon = 0;
+
+  for (let i = 0; i < geohash.length; i++) {
+    const ch = BASE32.indexOf(geohash[i]);
+    for (let j = 0; j < 5; j++) {
+      const mask = BITS[j];
+      if (isEven) {
+        if ((ch & mask) !== 0) {
+          lonMin = (lonMin + lonMax) / 2;
+        } else {
+          lonMax = (lonMin + lonMax) / 2;
+        }
+      } else {
+        if ((ch & mask) !== 0) {
+          latMin = (latMin + latMax) / 2;
+        } else {
+          latMax = (latMin + latMax) / 2;
+        }
+      }
+      isEven = !isEven;
+    }
+  }
+
+  lat = (latMin + latMax) / 2;
+  lon = (lonMin + lonMax) / 2;
+
+  return [lat, lon];
 }
